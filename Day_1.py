3d4a25b705f12b2c6e96cf9bc987918722fad81d
Rutuja Jain <76470444+rutujaJain-gitHub@users.noreply.github.com>
2021-08-18 23:12:26 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,28 @@
+print("Hello world!")
+print("print('what to print')")
+print("Hello" + " " + "World")
+print("Hello" + " " + "World")
+print("Hello" + " " + "World")
+print('String concatination is done by adding "+" this sign')
+# input("What is your name?")
+print("Hello" + " " + input("What's your name?") + "!!")
+
+print(len(input("Where do you live? ")))
+
+name = input("Which is your fav color? ")
+print(name)
+print(len(name))
+
+
+a = input("a: ")
+b = input("b: ")
+
+c=a
+a=b
+b=c
+
+a = print("a= " +a)
+b= print("b= " +b)
+
+
+
