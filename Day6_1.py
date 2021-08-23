841f960c056b687a6aab536af7834fbb3fa787f0
Rutuja Jain <76470444+rutujaJain-gitHub@users.noreply.github.com>
2021-08-23 23:58:44 +0530

    Add files via upload

---
+++ Day6_1.py
@@ -0,0 +1,21 @@
+
+def turn_left():
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+
+def move():
+    print(".  .  .  .  .")
+
+
+def turn_right():
+    print(".")
+    print(".")
+    print(".")
+    print(".")
+
+
+turn_left()
+move()
+turn_right()
