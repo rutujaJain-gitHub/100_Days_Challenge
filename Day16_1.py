51299e5c3a495d92b6c8a30fc1b2e2e18be76b33
Rutuja Jain <76470444+rutujaJain-gitHub@users.noreply.github.com>
2021-09-03 00:01:22 +0530

    Add files via upload

---
+++ Day16_1.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(1000)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
