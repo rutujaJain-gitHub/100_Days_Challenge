66778b99eb8596dd5163348a266197d4deb3bbc0
Rutuja Jain <76470444+rutujaJain-gitHub@users.noreply.github.com>
2021-09-10 23:55:19 +0530

    Add files via upload

---
+++ Day24_1.py
@@ -0,0 +1,10 @@
+#with open("my_file.txt") as file:
+ #   contents = file.read()
+  #  print(contents)
+
+#with open("my_file.txt", mode="w") as file:
+ #   file.write("\nNew text.")
+
+with open("/Users/pc/Desktop/testing.txt") as file:
+    contents = file.read()
+    print(contents)
