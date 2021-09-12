7e375f2ffc94af1182d080bd9821f6ff77a0bd48
Rutuja Jain <76470444+rutujaJain-gitHub@users.noreply.github.com>
2021-09-12 23:13:06 +0530

    Add files via upload

---
+++ Day26_1.py
@@ -0,0 +1,4 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 2, 34, 55]
+
+squared_numbers = [num ** 2 for num in numbers]
+print(squared_numbers)
---
+++ Day26_4.py
@@ -0,0 +1,5 @@
+sentence = "What is the Airspeed Velocity of an Unland Swallow?"
+
+result = {word : len(word) for word in sentence.split()}
+
+print(result)
